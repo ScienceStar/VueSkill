1af265d01e618d5437def41fa812c75dc30dfe40
mac <1633696870@qq.com>
2020-09-13 16:14:11 +0800

    vue 事件指令测试

--- vuedemo/src/router/index.js
+++ vuedemo/src/router/index.js
@@ -28,7 +28,7 @@ export default new Router({
 //docker run --name jenkins -d -p 80:8080 -p 50000:50000 -v /docker_data/jenkins_data/:/var/jenkins_home -u root myjenk
 //sed -i 's/http:\/\/updates.jenkins-ci.org\/download/https:\/\/mirrors.tuna.tsinghua.edu.cn\/jenkins/g' /var/jenkins_home/updates/default.json
 //sed -i 's/http:\/\/www.google.com/https:\/\/www.baidu.com/g' /var/jenkins_home/updates/default.json
-//1a69cf0fcd18984169bd1bcc1b05c8b322fb1083
+//1a69cf0fcd18984169bd1bcc1b05c8b322fb1083 341d71513k.wicp.vip:34249  341d71513k.wicp.vip:34249
 
 
 
