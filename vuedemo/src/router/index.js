d36591b3a248bfaef5a1f8236dcf09197801887b
mac <1633696870@qq.com>
2020-09-14 21:47:28 +0800

    vue 事件指令测试

--- vuedemo/src/router/index.js
+++ vuedemo/src/router/index.js
@@ -14,7 +14,7 @@ export default new Router({
       component: HelloWorld
     },
     {
-      path: '/welcome12',
+      path: '/welcome',
       name: 'welcome',
       component: welcome
     },
